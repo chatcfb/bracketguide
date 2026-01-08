6836ec521615adc7ba184fcae832abfafbcf8935
matthew.l.ingram <matthew.l.ingram@gmail.com>
2026-01-08 21:15:39 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,21 +1,23 @@
 import Create from './pages/Create';
+import DailyPickEm from './pages/DailyPickEm';
 import Explore from './pages/Explore';
 import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import MyChannel from './pages/MyChannel';
 import Profile from './pages/Profile';
-import DailyPickEm from './pages/DailyPickEm';
+import Landing from './pages/Landing';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Create": Create,
+    "DailyPickEm": DailyPickEm,
     "Explore": Explore,
     "Home": Home,
     "Marketplace": Marketplace,
     "MyChannel": MyChannel,
     "Profile": Profile,
-    "DailyPickEm": DailyPickEm,
+    "Landing": Landing,
 }
 
 export const pagesConfig = {
