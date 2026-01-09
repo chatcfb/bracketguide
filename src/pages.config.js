f73541200c5270903bd49a80bd80ea3d3780166e
matthew.l.ingram <matthew.l.ingram@gmail.com>
2026-01-09 07:19:58 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,10 +2,11 @@ import Create from './pages/Create';
 import DailyPickEm from './pages/DailyPickEm';
 import Explore from './pages/Explore';
 import Home from './pages/Home';
+import Landing from './pages/Landing';
 import Marketplace from './pages/Marketplace';
 import MyChannel from './pages/MyChannel';
 import Profile from './pages/Profile';
-import Landing from './pages/Landing';
+import LogoDemo from './pages/LogoDemo';
 import __Layout from './Layout.jsx';
 
 
@@ -14,10 +15,11 @@ export const PAGES = {
     "DailyPickEm": DailyPickEm,
     "Explore": Explore,
     "Home": Home,
+    "Landing": Landing,
     "Marketplace": Marketplace,
     "MyChannel": MyChannel,
     "Profile": Profile,
-    "Landing": Landing,
+    "LogoDemo": LogoDemo,
 }
 
 export const pagesConfig = {
---
+++ src/pages/LogoDemo.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import LogoDemo from '@/components/ui/LogoDemo';
+
+export default function LogoDemoPage() {
+  return <LogoDemo />;
+}
